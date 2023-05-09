8fc53fe824b92001817366668aef19e0198dde1a
WmW <zhouzheng0646@qq.com>
2023-05-10 02:24:13 +0800

    add the function  `create_snapshot`

---
+++ snapshot/js/fetch.js
@@ -0,0 +1,4 @@
+
+async function fetch() {
+  print("fetch")
+}
---
+++ snapshot/mod.rs
@@ -0,0 +1,59 @@
+use deno_core::{include_ascii_string, Extension, JsRuntime, RuntimeOptions};
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+use crate::ops::{fetch, log};
+
+// const OPS_JS_FILE: &[&str] = &["js/**/*.js"];
+
+pub fn create_snapshot() {
+    let ops_file = read_directory("js/").unwrap();
+
+    let options = RuntimeOptions {
+        will_snapshot: true,
+        // module_loader: Some(Rc::new(FsModuleLoader)),
+        extensions: vec![Extension::builder("snapshot")
+            .ops(vec![log::log::decl(), fetch::fetch::decl()])
+            .build()],
+        ..Default::default()
+    };
+    let mut runtime = JsRuntime::new(options);
+
+    // 将每个 op 的 js 代码注入到 snapshot 中；提供一层 js 包装的 api
+    // name的作用：在 js 中可以通过 Deno.core.ops()[name] 获取到对应的 op;
+    for opf in ops_file {
+        let op_file: String = opf.path().to_str().unwrap().to_string();
+        let op_name = op_file.replace("js/", "").replace(".js", "");
+        let op_name: Box<str> = op_name.into_boxed_str();
+        let op_name: &'static str = Box::leak(op_name);
+        println!("【 op name 】==> {:?}", op_name);
+
+        let op_code = fs::read_to_string(&op_file).expect("failed to read file");
+        let op_code: Box<str> = op_code.into_boxed_str();
+        let op_code: &'static str = Box::leak(op_code);
+        runtime.execute_script_static(op_name, op_code).unwrap();
+    }
+
+    // runtime
+    //     .execute_script("log", include_ascii_string!("js/log.js"))
+    //     .expect("Failed execute function the execute_script");
+
+    // runtime.execute_script_static("fe", r#"console.log(213)").unwrap();
+
+    let snapshot = runtime.snapshot();
+    // 将 snapshot 保存到文件中，写入二进制到文件中
+    fs::write("snapshot.bin", snapshot).expect("failed to write snapshot the file: ./snapshot.bin");
+
+    println!("snapshot created done, the file snapshot.bin");
+}
+
+fn read_directory(path: &str) -> Result<Vec<DirEntry>, std::io::Error> {
+    let mut entries = Vec::new();
+    let path = Path::new(path);
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            entries.push(entry?);
+        }
+    }
+    Ok(entries)
+}
